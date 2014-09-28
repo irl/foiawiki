c4995dfcc86307d3f9836e5ce985d235f691198a
Iain R. Learmonth <irl@fsfe.org>
2014-09-28 01:25:21 +0100

    Later version of scrapers + d2rq config

--- scraper-eastlothian.py
+++ scraper-eastlothian.py
@@ -2,8 +2,8 @@
 import urllib2, re, urlparse
 from bs4 import BeautifulSoup
 import mysql.connector
+from datetime import datetime
 
-cnx = mysql.connector.connect(user='foiawiki', password='579dVZq8BBa3XRcc', database='foiawiki')
 
 html = urllib2.urlopen("http://www.eastlothian.gov.uk/site/custom_scripts/foi_download_index.php?currentPage=1&itemsPerPage=2000000").read()
 
@@ -14,6 +14,9 @@ soup = BeautifulSoup(html)
 rows = soup.table.findAll('tr')
 
 for x in range(1, len(soup.table.findAll('tr'))):
+
+        cnx = mysql.connector.connect(user='foiawiki', password='579dVZq8BBa3XRcc', database='foiawiki')
+
         cols = rows[x].findAll('td')
         if ( re.search(datePattern, cols[0].string) is not None ):
             title = re.sub(datePattern, '', cols[0].string).strip()
@@ -27,17 +30,26 @@ for x in range(1, len(soup.table.findAll('tr'))):
 
             cursor = cnx.cursor()
 
-            query = "SELECT * FROM request WHERE `publicBodyId` = 2 AND `requestId` = %d"
-            cursor.execute(query, (iden))
-            if cursor.rowcount > 0:
+            query = "SELECT * FROM request WHERE publicBodyId = 2 AND requestId = %s"
+            cursor.execute(query, (iden,))
+            if cursor.fetchone() != None:
+                print "ROW RETURNED!"
                 cursor.close()
                 continue
 
-            query = "INSERT INTO request VALUES (%s, %s, NULL, %s, %s, %s, NULL)"
-
             timestamp = datetime.strptime(date, "%B %Y")
+            query = "INSERT INTO request VALUES (%s, %s, NULL, %s, %s, %s, NULL)" 
 
-            cursor.execute(query, (2, iden, title, timestamp, cols[0].a['href']))
+            cursor.close()
+            cnx.close()
+            cnx = mysql.connector.connect(user='foiawiki', password='579dVZq8BBa3XRcc', database='foiawiki')
+            cursor = cnx.cursor()
+            cursor.execute(query, ('2', iden, title, timestamp, cols[0].a['href']))
+            for tag in tags:
+                cursor.execute("INSERT INTO requestTag VALUES (%s, %s)", (iden, tag))
+            cnx.commit()
+            cursor.close()
+        
 
             print "ID: %s" % (iden,)
             print "Title: %s" % (title,)
